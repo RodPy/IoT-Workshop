5184914259da3b2e2e456894005ca1646212f9aa
RodPy <rodneyrrspy@gmail.com>
2022-04-25 17:50:29 -0400

    Upgrade

--- pythonFiles/DHT11.py
+++ pythonFiles/DHT11.py
@@ -3,7 +3,7 @@ import time
 
 while True:
     sensor = Adafruit_DHT.DHT11 #Cambia por DHT22 y si usas dicho sensor
-    pin = 16 #Pin en la raspberry donde conectamos el sensor
+    pin = 12 #Pin en la raspberry donde conectamos el sensor
     humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
 
     print ('Humedad: ' , humedad)
--- pythonFiles/led.py
+++ pythonFiles/led.py
@@ -1,10 +1,18 @@
-
+#
+"""
+    https://learn.sparkfun.com/tutorials/raspberry-gpio/python-rpigpio-api
+    https://roboticadiy.com/how-to-blink-led-with-raspberry-pi-4/
+"""
 import RPi.GPIO as GPIO 
 import time
 
-LED_PIN = 2
+LED_PIN = 2 #BCM
+# LED_PIN = 3 #BOARD
+
+
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BCM) 
+# GPIO.setmode(GPIO.BOARD)
 GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW) 
 
 
