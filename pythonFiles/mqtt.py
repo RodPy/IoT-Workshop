58aa112506505e594855cce44b33727dcf7ec8b6
RodPy <rodneyrrspy@gmail.com>
2022-06-19 23:10:49 -0400

    fix

--- pythonFiles/mqtt.py
+++ pythonFiles/mqtt.py
@@ -1,22 +1,24 @@
 
 from paho.mqtt import client as mqtt_client
-
 import random
 import time
-broker = '192.168.0.3'
-port = 1883
-topic = "rele1"
-# generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 100)}'
 
+broker = '172.20.10.3'
+port = 1883
+
+## Topic 
+bano = "rele1"
+sala = "rele2"
+cocina = "rele3"
+dormitorio = "rele4"
+client_id = "RodPy"
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
-            print("Connected to MQTT Broker!")
+            print("Conectado a MQTT Broker!")
         else:
-            print("Failed to connect, return code %d\n", rc)
-
+            print("Conexion fallida,  code %d\n", rc)
     client = mqtt_client.Client(client_id)
     client.on_connect = on_connect
     client.connect(broker, port)
@@ -29,15 +31,13 @@ def publish(client):
         time.sleep(1)
         if msg_count%2==0: msg="true"
         else : msg ="false"
-        
-        result = client.publish(topic, msg)
-        print (result)
-        
+        result = client.publish(bano, msg)
+        print (result)        
         status = result[0] # 0 cuando logra enviar
         if status == 0:
-            print(f"Enviando `{msg}` al topic `{topic}`")
+            print(f"Enviando `{msg}` al topic `{bano}`")
         else:
-            print(f"Mensaje no enviado al topic -> {topic}")
+            print(f"Mensaje no enviado al topic -> {bano}")
         msg_count += 1
 
 
