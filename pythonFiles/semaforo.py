a155a2e8d63d2743d96ffeae5effd53026a6ed35
rrojaspy <rrojas@paraguayeduca.org>
2022-06-22 10:20:59 -0400

    update

--- pythonFiles/semaforo.py
+++ pythonFiles/semaforo.py
@@ -1,13 +1,47 @@
-import Adafruit_DHT
-import time 
-sensor = Adafruit_DHT.DHT11
-pin =21
-3## TEMP mayor a 25 ROJO, MENOR A 25 AMARILLO y MAYOR A 20, 
-# MENOR A 20 VERDE
-while True:
-    lectura =Adafruit_DHT.read_retry(sensor,pin)
-    humedad=lectura[0]
-    temperatura=lectura[1]
+import RPi.GPIO as GPIO # Libreria para manejo de pines 
+import time
 
-    print("La Humedad es: "+ str(humedad) + ". La Temp es: " + str(temperatura) )
-    time.sleep(5)
+# Los Pines de conexion GPIO 16, GPIO 20, GPIO 21
+LED_Azul = 16
+LED_Rojo = 20
+LED_Verde = 21
+
+GPIO.setwarnings(False) 
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(LED_Azul , GPIO.OUT)
+GPIO.setup(LED_Rojo , GPIO.OUT) 
+GPIO.setup(LED_Verde, GPIO.OUT) 
+
+# Funcion para encender Led Amarillo
+def encenderledAmarilo():
+    GPIO.output(LED_Rojo, GPIO.HIGH) 
+    GPIO.output(LED_Verde, GPIO.HIGH)
+    GPIO.output(LED_Azul, GPIO.HIGH) 
+    
+# Funcion para apagar Led Amarillo
+def apargarledAmarillo():
+    GPIO.output(LED_Rojo, GPIO.LOW) 
+    GPIO.output(LED_Verde, GPIO.LOW)
+    GPIO.output(LED_Azul, GPIO.LOW) 
+
+
+    
+while True:
+    ## Rojo
+    GPIO.output(LED_Rojo, GPIO.HIGH) 
+    time.sleep(1) 
+    GPIO.output(LED_Rojo, GPIO.LOW) 
+    time.sleep(1)
+    
+    ## Amarillo
+    apargarledAmarillo()
+    time.sleep(2)
+    apargarledAmarillo()
+    time.sleep(2)
+
+    ## Verde  
+    time.sleep(3)
+    GPIO.output(LED_Verde, GPIO.HIGH) 
+    time.sleep(3) 
+    GPIO.output(LED_Verde, GPIO.LOW) 
+    time.sleep(3)
