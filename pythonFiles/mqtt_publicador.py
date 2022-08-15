04b4408e8e069773b4752b2411cbe7d0f7e0d691
Rodney <rodneyrrspy@gmail.com>
2022-08-14 23:47:38 -0400

    update

--- pythonFiles/mqtt_publicador.py
+++ pythonFiles/mqtt_publicador.py
@@ -1,16 +1,27 @@
 import paho.mqtt.client as mqtt
+import Adafruit_DHT  # Libreria para uso de sensor
+from time import sleep
 
 ## Configuracion de servidor 
-servidor_ip = "192.168.0.27" ## Para publicar en otro dispositivo
-#servidor_ip = "localhost"   ## Para publicar a nuestro dispositivo
+# servidor_ip = "192.168.100.XXX" ## Para publicar en otro dispositivo
+servidor_ip = "localhost"   ## Para publicar a nuestro dispositivo
 puerto=1883
-cliente='Publicador_Penguin'
+cliente='Penguin'
 
 ## Conexion al Servidor 
-client = mqtt.Client(cliente) # Creación del cliente
+client = mqtt.Client(cliente) # Creacion del cliente
 client.connect(servidor_ip,puerto)
 
 ## Creacion de TOPICS
-topic = "casa/luz"
+topic1 = "mesa99/sensor/humedad"
+topic2 = "mesa99/sensor/temperatura"
 
-client.publish(topic, "Ejemplo desde Python")
+## Publicacion
+while True:
+    sensor = Adafruit_DHT.DHT11 
+    pin = 12 # Pin de conexion del sensor
+    humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
+    client.publish(topic1, str(humedad))
+    delay(5)
+    client.publish(topic1, str(temperatura))
+    delay(5)
--- pythonFiles/mqtt_suscriptor.py
+++ pythonFiles/mqtt_suscriptor.py
@@ -1,48 +1,28 @@
 import paho.mqtt.client as mqtt
+from time import sleep
 import RPi.GPIO as GPIO 
+led_verde = 20 #BCM
 
-
-servidor_ip = "192.168.0.27"
-puerto=1883
-cliente='Publicador_Penguin'
-
-
-#Topic de dispositivos
-topic1 = "casa/luz"
-
-#Funciones 
-def on_connect(client, userdata, flags, rc):
- print("Connected with result code " + str(rc))
- print("UserData= " + str(userdata))
- print("flags= " + str(flags))
- print("")
- client.subscribe(topic1)
-
-def on_message(client, userdata, message):
-    return(message.topic,str(message.payload.decode("utf-8")))
-    print("Mensaje recibido=", str(message.payload.decode("utf-8")))
-    # print("Topic=", message.topic)
-    # print("Nivel de calidad [0|1|2]=", message.qos)
-    # print("Flag de retención=", message.retain)
-    print("---------------------------------------------")
-    print("")
- 
-
-client = mqtt.Client(cliente, userdata="Grupo_1") 
-client.on_connect = on_connect 
-client.on_message = on_message 
-# Connecting to the MQTT broker.
-client.connect(servidor_ip, puerto, 60) 
-client.loop_forever()
-
-LED_PIN = 20 #BCM
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BCM) 
-GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setup(led_verde, GPIO.OUT, initial=GPIO.LOW) 
 
-if on_message[0]==topic1 and on_message[1]='ON':
-    GPIO.output(LED_PIN, GPIO.HIGH) 
-    time.sleep(1) 
-elif on_message[0]==topic1 and on_message[1]='OFF':   
-    GPIO.output(LED_PIN, GPIO.LOW) 
-    time.sleep(1)
+## Configuracion de servidor 
+# servidor_ip = "192.168.0.20" ## Para publicar en otro dispositivo
+servidor_ip = "localhost"   ## Para publicar a nuestro dispositivo
+puerto=1883
+cliente='Penguin'
+
+## Creacion de TOPICS
+topic1 = "mesa99/led/verde"
+
+#Funciones 
+def on_message(client, userdata, message):
+    print("Mensaje recibido=", str(message.payload.decode("utf-8")))
+    print("Topic=", message.topic)
+
+client = mqtt.Client(cliente) 
+client.on_message = on_message 
+client.connect(servidor_ip, puerto) 
+client.subscribe(topic1) # Subscripción al topic
+client.loop_forever()
